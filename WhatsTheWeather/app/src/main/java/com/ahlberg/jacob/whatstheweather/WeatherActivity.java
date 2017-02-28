efca718b6bf86416299857bd3fb378f5f6a3ddec
JacobAhlberg <jacahlberg@hotmail.com>
2017-02-28 14:16:45 +0100

    Google api client added

--- WhatsTheWeather/app/src/main/java/com/ahlberg/jacob/whatstheweather/WeatherActivity.java
+++ WhatsTheWeather/app/src/main/java/com/ahlberg/jacob/whatstheweather/WeatherActivity.java
@@ -1,6 +1,11 @@
 package com.ahlberg.jacob.whatstheweather;
 
 import android.app.DownloadManager;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,12 +15,17 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationServices;
 
 import org.json.JSONObject;
 
 
-public class WeatherActivity extends AppCompatActivity {
+public class WeatherActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener,
+        GoogleApiClient.ConnectionCallbacks, LocationListener
+{
 
     final String URL_BASE = "http://api.openweathermap.org/data/2.5/forecast";
     final String URL_COORDS = "/?lat=59.32800&lon=18.05795";
@@ -23,13 +33,19 @@ public class WeatherActivity extends AppCompatActivity {
     final String URL_API_KEY = "&APPID=6293987fb4d85b38ac93029090356751";
 
     private GoogleApiClient mGoogleApiClient;
+    private final int PERMISSION_LOCATION = 111;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
 
-
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .enableAutoManage(this, this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
 
     }
 
@@ -53,5 +69,31 @@ public class WeatherActivity extends AppCompatActivity {
         });
 
         Volley.newRequestQueue(this).add(jsonRequest);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+            
+        }
+
+
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
     }
 }
