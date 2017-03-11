6fcd18c5fd66adac0100ffa755dfde692920c7f1
JacobAhlberg <jacahlberg@hotmail.com>
2017-03-11 09:49:29 +0100

    updated

--- WhatsTheWeather/app/src/main/java/com/ahlberg/jacob/whatstheweather/WeatherAdapter.java
+++ WhatsTheWeather/app/src/main/java/com/ahlberg/jacob/whatstheweather/WeatherAdapter.java
@@ -1,33 +1,25 @@
 package com.ahlberg.jacob.whatstheweather;
 
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
-import com.ahlberg.jacob.whatstheweather.model.DailyWeatherReport;
-import com.ahlberg.jacob.whatstheweather.model.WeeklyWeather;
 import com.ahlberg.jacob.whatstheweather.model.WeeklyWeatherReport;
 
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
 
 /**
  * Created by Jacob on 2017-03-05.
  * What's The Weather?
- * SMHI API
+ * Darksky API
  */
 
 class WeatherAdapter extends RecyclerView.Adapter<WeatherReportViewHolder> {
     private Context context;
     private ArrayList<WeeklyWeatherReport> weeklyWeatherReports;
-    private int whichDay = 1;
-
 
     WeatherAdapter(ArrayList<WeeklyWeatherReport> weeklyWeatherReport, Context context) {
         this.weeklyWeatherReports = weeklyWeatherReport;
@@ -53,7 +45,6 @@ class WeatherAdapter extends RecyclerView.Adapter<WeatherReportViewHolder> {
         holder.tempHigh.setText(tempMax);
 
         holder.weatherDescription.setText(report.getSummary());
-
         String day = WeeklyWeatherReport.getDaysOfWeek(position + 1);
         holder.weatherDate.setText(day);
 
